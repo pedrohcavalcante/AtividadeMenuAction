416b929f275b9106cd738b93a31a7033367e1e4f
Android BTI <android.bti@A309-05L.imd.local>
2018-10-16 17:15:21 -0300

    Initial commit

---
+++ app/src/main/java/com/example/androidbti/atividademenuaction/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.androidbti.atividademenuaction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.actionAdd){
+            // TODO aqui o fragment
+            return true;
+        } else {
+            Toast.makeText(this, "Não foi possivel encontrar a opcao escolhida", Toast.LENGTH_SHORT).show();
+            return super.onOptionsItemSelected(item);
+        }
+    }
+}
